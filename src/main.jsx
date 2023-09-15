91a7a0199e5522b0cb78dbedf5820bca72e99851
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-09-15 17:12:01 +0330

    initial styles and neccessary data added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import theme from "./mui/theme";
-import "./styles/fonts.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
