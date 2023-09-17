e16d1d323c9f4763f2b8942c0c0e7cef7cb8ab41
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-09-18 02:59:41 +0330

    calendar Page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Typography, CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeCotext, useMode } from "../src/mui/theme";
 import AppBar from "../src/scenes/global/AppBar";
 import Dashboard from "../src/scenes/dashboard/Dashboard";
@@ -8,6 +8,7 @@ import Team from "../src/scenes/team/Team";
 import Contacts from "../src/scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
+import Calendar from "./scenes/calendar/Calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -25,6 +26,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
